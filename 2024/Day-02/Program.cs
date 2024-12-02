8a4a9abbcf19c8f3a9e4b17d4427424550b7f2a3
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-02 09:59:38 +0100

    Implement Part 1 logic for Day 2 and add sample input file

--- 2024/Day-02/Program.cs
+++ 2024/Day-02/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Advent of Code 2024: Day 1
 
+using System.Linq.Expressions;
+
 var lines = File.ReadAllLines("input.txt");
 
 System.Console.WriteLine($"Part 1: {Part1(lines)}");
@@ -7,7 +9,43 @@ System.Console.WriteLine($"Part 2: {Part2(lines)}");
 
 int Part1(string[] lines)
 {
-    return 0;
+    var countSafe = 0;
+
+    foreach (var line in lines)
+    {
+        var parts = line.Split(' ').Select(int.Parse).ToArray();
+
+        bool isIncreasing = true;
+        bool isDecreasing = true;
+
+        for (int i = 1; i < parts.Length; i++)
+        {
+            var diff = parts[i] - parts[i - 1];
+
+            if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+            {
+                isIncreasing = false;
+                isDecreasing = false;
+                break;
+            }
+
+            if (diff < 0)
+            {
+                isIncreasing = false;
+            }
+            else if (diff > 0)
+            {
+                isDecreasing = false;
+            }
+        }
+
+        if (isIncreasing || isDecreasing)
+        {
+            countSafe++;
+        }
+    }
+
+    return countSafe;
 }
 
 
