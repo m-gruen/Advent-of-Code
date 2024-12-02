476b0e38bd0ab4324b84b4d43f0ad45e9c5b511e
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-02 10:11:57 +0100

    Refactor Part 2 of Day 2 logic to enhance sequence checking and improve safety count

--- 2024/Day-02/Program.cs
+++ 2024/Day-02/Program.cs
@@ -15,31 +15,7 @@ int Part1(string[] lines)
     {
         var parts = line.Split(' ').Select(int.Parse).ToArray();
 
-        bool isIncreasing = true;
-        bool isDecreasing = true;
-
-        for (int i = 1; i < parts.Length; i++)
-        {
-            var diff = parts[i] - parts[i - 1];
-
-            if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
-            {
-                isIncreasing = false;
-                isDecreasing = false;
-                break;
-            }
-
-            if (diff < 0)
-            {
-                isIncreasing = false;
-            }
-            else if (diff > 0)
-            {
-                isDecreasing = false;
-            }
-        }
-
-        if (isIncreasing || isDecreasing)
+        if (CheckSequence(parts))
         {
             countSafe++;
         }
@@ -48,8 +24,59 @@ int Part1(string[] lines)
     return countSafe;
 }
 
-
 int Part2(string[] lines)
 {
-    return 0;
+    var countSafe = 0;
+
+    foreach (var line in lines)
+    {
+        var parts = line.Split(' ').Select(int.Parse).ToArray();
+
+        if (CheckSequence(parts))
+        {
+            countSafe++;
+        } else {
+            for (int i = 0; i < parts.Length; i++)
+            {
+                var newParts = parts.Where((source, index) => index != i).ToArray();
+
+                if (CheckSequence(newParts))
+                {
+                    countSafe++;
+                    break;
+                }
+            }
+        }
+    }
+
+    return countSafe;
+}
+
+bool CheckSequence(int[] parts)
+{
+    bool isIncreasing = true;
+    bool isDecreasing = true;
+
+    for (int i = 1; i < parts.Length; i++)
+    {
+        var diff = parts[i] - parts[i - 1];
+
+        if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+        {
+            isIncreasing = false;
+            isDecreasing = false;
+            break;
+        }
+
+        if (diff < 0)
+        {
+            isIncreasing = false;
+        }
+        else if (diff > 0)
+        {
+            isDecreasing = false;
+        }
+    }
+
+    return isIncreasing || isDecreasing;
 }
