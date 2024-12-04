d47e82486ba6decefc3b9f16d7ce3ff81396c4bd
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-04 18:57:01 +0100

    Refactor Day 4 logic: streamline grid creation and impemented Part 2 functionality (does not fully work)

--- CSharp/2024/Day-04/Program.cs
+++ CSharp/2024/Day-04/Program.cs
@@ -2,38 +2,22 @@
 
 using System.Globalization;
 using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
-var lines = File.ReadAllLines("input.txt");
+var lines = File.ReadAllLines("input-short.txt");
 
 System.Console.WriteLine($"Part 1: {Part1(lines)}");
 System.Console.WriteLine($"Part 2: {Part2(lines)}");
 
 int Part1(string[] lines)
 {
-    char[,] grid = new char[lines.Length, lines[0].Length];
-    for (int i = 0; i < lines.Length; i++)
-    {
-        for (int j = 0; j < lines[i].Length; j++)
-        {
-            grid[i, j] = lines[i][j];
-        }
-    }
-
+    var grid = makeGrid(lines);
+    var directions = getDirection();
     int rows = grid.GetLength(0);
     int cols = grid.GetLength(1);
+
     var XmasCount = 0;
     string word = "XMAS";
-    (int, int)[] directions =
-    [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1)
-    ];
 
     for (int r = 0; r < rows; r++)
     {
@@ -61,5 +45,83 @@ int Part1(string[] lines)
 
 int Part2(string[] lines)
 {
-    return 0;
+    var grid = makeGrid(lines);
+    (int, int)[] directions = [
+        (-1, -1),
+        (-1, 1),
+        (1, -1),
+        (1, 1)
+    ];
+
+    int rows = grid.GetLength(0);
+    int cols = grid.GetLength(1);
+
+    var XmasCount = 0;
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            if (grid[r, c] == 'A')
+            {
+                var mCount = 0;
+                var sCount = 0;
+                var sPosition = new List<(int, int)>();
+                var mPosition = new List<(int, int)>();
+
+                foreach (var (dr, dc) in directions)
+                {
+                    int rd = r, cd = c;
+
+                    if (rd < 0 || rd >= rows || cd < 0 || cd >= cols) { break; }
+                    if (grid[rd, cd] == 'M')
+                    {
+                        mCount++;
+                        mPosition.Add((rd, cd));
+                    }
+                    if (grid[rd, cd] == 'S')
+                    {
+                        sCount++;
+                        sPosition.Add((rd, cd));
+                    }
+                    rd += dr;
+                    cd += dc;
+                }
+
+                if (mCount == 2 && sCount == 2)
+                {
+                    XmasCount++;
+                }
+            }
+        }
+    }
+
+    return XmasCount;
+}
+
+char[,] makeGrid(string[] lines)
+{
+    char[,] grid = new char[lines.Length, lines[0].Length];
+    for (int i = 0; i < lines.Length; i++)
+    {
+        for (int j = 0; j < lines[i].Length; j++)
+        {
+            grid[i, j] = lines[i][j];
+        }
+    }
+    return grid;
+}
+
+(int, int)[] getDirection()
+{
+    return [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1)
+    ];
 }
