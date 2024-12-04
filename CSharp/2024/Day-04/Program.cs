b464b527cf490bfeb3ca3b80a1ead9e895b52dc8
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-04 12:26:26 +0100

    Implement Part 1 logic for Day 4 of Advent of Code 2024.

--- CSharp/2024/Day-04/Program.cs
+++ CSharp/2024/Day-04/Program.cs
@@ -1,5 +1,8 @@
 ﻿// Advent of Code 2024 Day 4
 
+using System.Globalization;
+using System.Text.RegularExpressions;
+
 var lines = File.ReadAllLines("input.txt");
 
 System.Console.WriteLine($"Part 1: {Part1(lines)}");
@@ -7,7 +10,53 @@ System.Console.WriteLine($"Part 2: {Part2(lines)}");
 
 int Part1(string[] lines)
 {
-    return 0;
+    char[,] grid = new char[lines.Length, lines[0].Length];
+    for (int i = 0; i < lines.Length; i++)
+    {
+        for (int j = 0; j < lines[i].Length; j++)
+        {
+            grid[i, j] = lines[i][j];
+        }
+    }
+
+    int rows = grid.GetLength(0);
+    int cols = grid.GetLength(1);
+    var XmasCount = 0;
+    string word = "XMAS";
+    (int, int)[] directions =
+    [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1)
+    ];
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            if (grid[r, c] == 'X')
+            {
+                foreach (var (dr, dc) in directions)
+                {
+                    int rd = r, cd = c, k;
+                    for (k = 0; k < word.Length; k++)
+                    {
+                        if (rd < 0 || rd >= rows || cd < 0 || cd >= cols || grid[rd, cd] != word[k]) { break; }
+                        rd += dr;
+                        cd += dc;
+                    }
+                    if (k == word.Length) { XmasCount++; }
+                }
+            }
+        }
+    }
+
+    return XmasCount;
 }
 
 int Part2(string[] lines)
