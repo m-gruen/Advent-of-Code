12307a0ee498a3480d19fba5a1b058ea3d46757b
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-05 15:24:49 +0100

    Implement Part 2 logic for Day 5.

--- CSharp/2024/Day-05/Program.cs
+++ CSharp/2024/Day-05/Program.cs
@@ -23,8 +23,34 @@ int Part2(string[][] lines)
     var rules = getRules(lines[0]);
     var numberLines = getNumberLines(lines[1]);
 
-    
-    return 0;
+    var incorrectNumberLines = numberLines
+        .Where(numberLine => !IsValid(numberLine, rules))
+        .ToArray();
+
+    var rulesDict = new Dictionary<int, HashSet<int>>();
+    foreach (var rule in rules)
+    {
+        if (!rulesDict.TryGetValue(rule[0], out HashSet<int>? value))
+        {
+            value = [];
+            rulesDict[rule[0]] = value;
+        }
+
+        value.Add(rule[1]);
+    }
+
+    int Compare(int a, int b)
+    {
+        if (rulesDict.TryGetValue(a, out HashSet<int>? value1) && value1.Contains(b)) { return -1; }
+        if (rulesDict.TryGetValue(b, out HashSet<int>? value2) && value2.Contains(a)) { return 1; }
+        return 0;
+    }
+
+    foreach (var incorrectNumberLine in incorrectNumberLines) { Array.Sort(incorrectNumberLine, Compare); }
+
+    return incorrectNumberLines
+        .Where(numberLine => IsValid(numberLine, rules))
+        .Sum(numberLine => numberLine[numberLine.Length / 2]);
 }
 
 bool IsValid(int[] numberLine, int[][] rules)
@@ -43,15 +69,15 @@ bool IsValid(int[] numberLine, int[][] rules)
 int[][] getRules(string[] lines)
 {
     return [.. lines
-            .Select(l => l.Split('|')
-            .Select(int.Parse)
-            .ToArray())];
+        .Select(l => l.Split('|')
+        .Select(int.Parse)
+        .ToArray())];
 }
 
 int[][] getNumberLines(string[] lines)
 {
     return [.. lines
-            .Select(l => l.Split(',')
-            .Select(int.Parse)
-            .ToArray())];
+        .Select(l => l.Split(',')
+        .Select(int.Parse)
+        .ToArray())];
 }
