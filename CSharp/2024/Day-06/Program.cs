df4f41fcbca5dda25e5770c305c9758737a6cbe1
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-06 12:21:37 +0100

    Implement Part 2 logic for Day 6 of Advent of Code 2024

--- CSharp/2024/Day-06/Program.cs
+++ CSharp/2024/Day-06/Program.cs
@@ -25,7 +25,44 @@ int Part1(string[] lines)
 
 int Part2(string[] lines)
 {
-    return 0;
+    var grid = makeGrid(lines);
+    var start = GetStartingPos(grid);
+
+    var loopCount = 0;
+
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[0].Length; j++)
+        {
+            if (grid[i][j] != '^')
+            {
+                var temp = grid[i][j];
+                grid[i][j] = '#';
+
+                var visited = new HashSet<(int, int, Dir)>();
+                var pos = (start.Item1, start.Item2, Dir.Up);
+                Direction.currentDir = Dir.Up;
+
+                while (isInside(grid, pos.Item1, pos.Item2))
+                {
+                    if (visited.Contains(pos)) { loopCount++; break; }
+                    visited.Add(pos);
+                    var nextPos = Direction.GetNextPosAndDir(pos.Item1, pos.Item2);
+                    if (!isInside(grid, nextPos.Item1, nextPos.Item2)) { break; }
+                    else if (grid[nextPos.Item1][nextPos.Item2] == '#') 
+                    { 
+                        Direction.ChangeDir(); 
+                        pos = (pos.Item1, pos.Item2, Direction.currentDir);
+                    }
+                    else { pos = nextPos; }
+                }
+
+                grid[i][j] = temp;
+            }
+        }
+    }
+
+    return loopCount;
 }
 
 char[][] makeGrid(string[] lines) => lines.Select(line => line.ToCharArray()).ToArray();
@@ -47,7 +84,7 @@ bool isInside(char[][] grid, int i, int j) => i >= 0 && i < grid.Length && j >= 
 
 class Direction
 {
-    private static Dir currentDir = Dir.Up;
+    public static Dir currentDir = Dir.Up;
     public static (int, int) GetNextPos(int i, int j)
     {
         return currentDir switch
@@ -57,6 +94,18 @@ class Direction
             Dir.Left => (i, j - 1),
             Dir.Right => (i, j + 1),
             _ => (0, 0),
+        };
+    }
+
+    public static (int, int, Dir) GetNextPosAndDir(int i, int j)
+    {
+        return currentDir switch
+        {
+            Dir.Up => (i - 1, j, Dir.Up),
+            Dir.Down => (i + 1, j, Dir.Down),
+            Dir.Left => (i, j - 1, Dir.Left),
+            Dir.Right => (i, j + 1, Dir.Right),
+            _ => (0, 0, Dir.Up),
         };
     }
 
@@ -71,12 +120,12 @@ class Direction
             _ => Dir.Up,
         };
     }
+}
 
-    private enum Dir
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    }
+enum Dir
+{
+    Up,
+    Down,
+    Left,
+    Right
 }
