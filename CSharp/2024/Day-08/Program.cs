b7ba7bfa9952c9a7d175917eb1cda930e2611707
m-gruen <mark.gruenzweil@gmail.com>
2024-12-08 15:02:28 +0100

    Add initial implementation for Day 8 of Advent of Code 2024 with project files, input data, and basic structure

---
+++ CSharp/2024/Day-08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Advent of Code 2024 Day 7
+
+var lines = File.ReadAllLines("input-short.txt");
+
+System.Console.WriteLine($"Part 1: {Part1(lines)}");
+System.Console.WriteLine($"Part 2: {Part2(lines)}");
+
+long Part1(string[] lines)
+{
+    return 0;
+}
+
+long Part2(string[] lines)
+{
+    return 0;
+}
