e66447356925c0b37616e070ef11a4e6605e4a24
m-gruen <mark.gruenzweil@gmail.com>
2024-12-09 16:00:29 +0100

    Refactor Part 1 logic by extracting instruction processing into a separate method

--- CSharp/2024/Day-09/Program.cs
+++ CSharp/2024/Day-09/Program.cs
@@ -1,20 +1,13 @@
 ﻿// Advent of Code 2024 Day 8
 
-var input = File.ReadAllText("input.txt"); 
+var input = File.ReadAllText("input.txt");
 
 System.Console.WriteLine($"Part 1: {Part1(input)}");
 System.Console.WriteLine($"Part 2: {Part2(input)}");
 
 long Part1(string input)
 {
-    var instructions = MakeIntArray(input);
-    var index = 0;
-    List<int> blocks = [];
-
-    for (int i = 0; i < instructions.Length; i++)
-    {
-        blocks.AddRange(Enumerable.Repeat(i % 2 == 0 ? index++ : -1, instructions[i]));
-    }
+    var blocks = ProcessInstructions(input);
 
     for (int i = blocks.Count - 1; i >= 0; i--)
     {
@@ -39,3 +32,17 @@ int Part2(string input)
 int[] MakeIntArray(string input) => [.. input.Select(c => int.Parse(c.ToString()))];
 
 long CalculateSum(int[] array) => array.Select((n, i) => (long)n * i).Sum();
+
+List<int> ProcessInstructions(string input)
+{
+    var instructions = MakeIntArray(input);
+    var index = 0;
+    List<int> blocks = new List<int>();
+
+    for (int i = 0; i < instructions.Length; i++)
+    {
+        blocks.AddRange(Enumerable.Repeat(i % 2 == 0 ? index++ : -1, instructions[i]));
+    }
+
+    return blocks;
+}
