97ff229a170140d68464420923432dee216f5f0b
m-gruen <mark.gruenzweil@gmail.com>
2024-12-10 19:05:17 +0100

    Implement Part 2 logic for Day 10 of Advent of Code 2024, including grid traversal and pathfinding

--- CSharp/2024/Day-10/Program.cs
+++ CSharp/2024/Day-10/Program.cs
@@ -45,9 +45,25 @@ int Part1(string[] lines)
     return count;
 }
 
-int Part2(string[] input)
+int Part2(string[] lines)
 {
-    return 0;
+    var grid = MakeGrid(lines);
+    var count = 0;
+
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[i].Length; j++)
+        {
+            if (grid[i][j] == 0)
+            {
+                int nineCount = 0;
+                FindPaths(grid, i, j, ref nineCount);
+                count += nineCount;
+            }
+        }
+    }
+
+    return count;
 }
 
 int[][] MakeGrid(string[] lines) => [.. lines.Select(line => line.Select(c => c - '0').ToArray())];
@@ -59,3 +75,25 @@ void CheckDirections(int[][] grid, Queue<(int, int)> queue, int x, int y)
     if (y > 0 && grid[x][y - 1] == grid[x][y] + 1) { queue.Enqueue((x, y - 1)); }
     if (y < grid[x].Length - 1 && grid[x][y + 1] == grid[x][y] + 1) { queue.Enqueue((x, y + 1)); }
 }
+
+void FindPaths(int[][] grid, int x, int y, ref int count)
+{
+    if (grid[x][y] == 9)
+    {
+        count++;
+        return;
+    }
+
+    int[][] directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    foreach (var dir in directions)
+    {
+        int newX = x + dir[0];
+        int newY = y + dir[1];
+        if (IsInGrid(grid, newX, newY) && grid[newX][newY] == grid[x][y] + 1)
+        {
+            FindPaths(grid, newX, newY, ref count);
+        }
+    }
+}
+
+bool IsInGrid(int[][] grid, int x, int y) => x >= 0 && x < grid.Length && y >= 0 && y < grid[x].Length;
