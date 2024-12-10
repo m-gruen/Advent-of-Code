999805284f06c057d5a628a1f5b5be90a61f5611
m-gruen <mark.gruenzweil@gmail.com>
2024-12-10 18:19:53 +0100

    Implement Part 1 logic for Day 10 of Advent of Code 2024, including grid creation and traversal logic

--- CSharp/2024/Day-10/Program.cs
+++ CSharp/2024/Day-10/Program.cs
@@ -1,16 +1,61 @@
 ﻿// Advent of Code 2024 Day 10
 
-var input = File.ReadAllText("input-short.txt");
+var lines = File.ReadAllLines("input.txt");
 
-System.Console.WriteLine($"Part 1: {Part1(input)}");
-System.Console.WriteLine($"Part 2: {Part2(input)}");
+System.Console.WriteLine($"Part 1: {Part1(lines)}");
+System.Console.WriteLine($"Part 2: {Part2(lines)}");
 
-int Part1(string input)
+int Part1(string[] lines)
+{
+    var grid = MakeGrid(lines);
+
+    var count = 0;
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[i].Length; j++)
+        {
+            if (grid[i][j] == 0)
+            {
+                HashSet<(int, int)> visited = [];
+                Queue<(int, int)> queue = [];
+                queue.Enqueue((i, j));
+
+                while (queue.Count > 0)
+                {
+                    var (x, y) = queue.Dequeue();
+
+                    if (!visited.Contains((x, y)))
+                    {
+                        visited.Add((x, y));
+
+                        if (grid[x][y] == 9)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            CheckDirections(grid, queue, x, y);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+int Part2(string[] input)
 {
     return 0;
 }
 
-int Part2(string input)
+int[][] MakeGrid(string[] lines) => [.. lines.Select(line => line.Select(c => c - '0').ToArray())];
+
+void CheckDirections(int[][] grid, Queue<(int, int)> queue, int x, int y)
 {
-    return 0;
+    if (x > 0 && grid[x - 1][y] == grid[x][y] + 1) { queue.Enqueue((x - 1, y)); }
+    if (x < grid.Length - 1 && grid[x + 1][y] == grid[x][y] + 1) { queue.Enqueue((x + 1, y)); }
+    if (y > 0 && grid[x][y - 1] == grid[x][y] + 1) { queue.Enqueue((x, y - 1)); }
+    if (y < grid[x].Length - 1 && grid[x][y + 1] == grid[x][y] + 1) { queue.Enqueue((x, y + 1)); }
 }
