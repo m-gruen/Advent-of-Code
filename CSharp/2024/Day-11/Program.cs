966299e368c74af23c6727b6eb07c593e57c8dc1
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-11 12:40:41 +0100

    Implement Part 2 logic for Day 11 of Advent of Code 2024 CSharp

--- CSharp/2024/Day-11/Program.cs
+++ CSharp/2024/Day-11/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Advent of Code 2024 Day 11
 
+using System.Globalization;
+
 var input = File.ReadAllText("input.txt");
 
 System.Console.WriteLine($"Part 1: {Part1(input)}");
@@ -7,7 +9,7 @@ System.Console.WriteLine($"Part 2: {Part2(input)}");
 
 int Part1(string input)
 {
-    var stones = CreateLongArray(input);
+    var stones = MakeArray(input);
 
     for (int i = 0; i < 25; i++)
     {
@@ -17,16 +19,23 @@ int Part1(string input)
     return stones.Length;
 }
 
-int Part2(string input)
+long Part2(string input)
 {
-    return 0;
+    var stones = MakeTupleArray(input);
+
+    for (int i = 0; i < 75; i++)
+    {
+        stones = ConvertTupleArray(stones);
+    }
+
+    return stones.Sum(stone => stone.count);
 }
 
-long[] CreateLongArray(string input) => [.. input.Split(' ').Select(long.Parse)];
+long[] MakeArray(string input) => [.. input.Split(' ').Select(long.Parse)];
 
 long[] ConvertArray(long[] stones)
-{    
-    var newStones = new List<long>();
+{
+    List<long> newStones = [];
 
     foreach (var stone in stones)
     {
@@ -51,4 +60,49 @@ long[] ConvertArray(long[] stones)
     }
 
     return [.. newStones];
+}
+
+(long number, long count)[] ConvertTupleArray((long number, long count)[] stones)
+{
+
+    List<(long, long)> newStones = [];
+
+    foreach (var (number, count) in stones)
+    {
+        if (number == 0) { newStones.Add((1, count)); }
+        else
+        {
+            int numDigits = (int)Math.Floor(Math.Log10(number) + 1);
+            if (numDigits % 2 == 0)
+            {
+                int halfDigits = numDigits / 2;
+                long divisor = (long)Math.Pow(10, halfDigits);
+                newStones.Add((number / divisor, count));
+                newStones.Add((number % divisor, count));
+            }
+            else { newStones.Add((number * 2024, count)); }
+        }
+    }
+
+
+    for (int i = 0; i < newStones.Count - 1; i++)
+    {
+        int j = i + 1;
+        while (j < newStones.Count)
+        {
+            if (newStones[i].Item1 == newStones[j].Item1)
+            {
+                newStones[i] = (newStones[i].Item1, newStones[i].Item2 + newStones[j].Item2);
+                newStones.RemoveAt(j);
+            }
+            else { j++; }
+        }
+    }
+
+    return [.. newStones];
+}
+
+(long number, long count)[] MakeTupleArray(string input)
+{
+    return [.. input.Split(' ').Select(long.Parse).Select(number => (number, 1L))];
 }
