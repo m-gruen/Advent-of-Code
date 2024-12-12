2e9e82f791ecb64084818f262a7a30f6f7b9236c
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-12 08:05:17 +0100

    Add initial implementation for Day 12 of Advent of Code 2024, including project files, program logic, and input data

---
+++ CSharp/2024/Day-12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Advent of Code 2024 Day 12
+
+var lines = File.ReadAllLines("input.txt");
+
+System.Console.WriteLine($"Part 1: {Part1(lines)}");
+System.Console.WriteLine($"Part 2: {Part2(lines)}");
+
+int Part1(string[] lines)
+{
+    return 0;
+}
+
+int Part2(string[] lines)
+{
+    return 0;
+}
